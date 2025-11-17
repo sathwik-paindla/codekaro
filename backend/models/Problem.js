f612261f2a818c7a37ab42b1137c26529a7114bc
sathwik-paindla <paindlasathwik@gmail.com>
2025-11-17 23:12:22 +0530

    Modified Problem Schema

--- backend/models/Problem.js
+++ backend/models/Problem.js
@@ -2,17 +2,48 @@
 const mongoose = require('mongoose');
 
 const testCaseSchema = new mongoose.Schema({
-    input: { type: String, required: true },
-    expectedOutput: { type: String, required: true }
+
+    input: { 
+        type: String, 
+        required: true 
+    },
+
+    expectedOutput: { 
+        type: String, 
+        required: true 
+    }
 });
 
 const problemSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    difficulty: { type: String, enum: ['Easy', 'Medium', 'Hard'], default: 'Easy' },
-    constraints: { type: String },
-    sampleInput: { type: String },
-    sampleOutput: { type: String },
+    
+    title: { 
+        type: String, 
+        required: true 
+    },
+
+    description: { 
+        type: String, 
+        required: true 
+    },
+
+    difficulty: { 
+        type: String, 
+        enum: ['Easy', 'Medium', 'Hard'], 
+        default: 'Easy' 
+    },
+
+    constraints: { 
+        type: String 
+    },
+
+    sampleInput: { 
+        type: String 
+    },
+
+    sampleOutput: { 
+        type: String 
+    },
+    
     testCases: [testCaseSchema]
 }, { timestamps: true });
 
