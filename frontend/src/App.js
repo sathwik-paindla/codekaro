143bc0b8675333a0ef3565be2e4dce3b3e5ebd8d
sathwik-paindla <paindlasathwik@gmail.com>
2025-11-29 17:22:45 +0530

    Modified layout UI with outlet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,28 @@ import { BrowserRouter as Router, Routes, Route, Navigate, BrowserRouter} from "
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Layout from "./components/Layout";
+import { useAuth } from "./context/AuthContext";
 
 
 
 export default function App() {
+  const {user}=useAuth();
 return (
+  
   <BrowserRouter>
-    <Layout>
+    
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route element={<Layout/>}>
+        <Route path="/" element={
+          <>
+          <h1 className="text-center mt-6 text-gray-600">Welcome,{user?.email}</h1>
+            </>
+          } />
+        </Route>
 
     </Routes>
-    </Layout>
    
   </BrowserRouter>
   
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";//for nested routes
 
-export default function Layout({ children }) {
+export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 bg-gray-50">{children}</main>
+      <main className="flex-1 bg-gray-50">
+        <Outlet/>
+      </main>
       <Footer />
     </div>
   );
