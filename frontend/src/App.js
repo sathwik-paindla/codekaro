5a14ca4117e616465a2f88564da1e5b14c0be7e4
sathwik-paindla <paindlasathwik@gmail.com>
2025-11-29 23:51:52 +0530

    Added get all problems route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import Layout from "./components/Layout";
 import { useAuth } from "./context/AuthContext";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import ProblemsList from "./pages/ProblemList";
 
 
 
@@ -13,15 +15,33 @@ return (
   <BrowserRouter>
     
       <Routes>
+        <Route path="/" element={<Navigate to="/problems" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
         <Route element={<Layout/>}>
+
         <Route path="/" element={
           <>
           <h1 className="text-center mt-6 text-gray-600">Welcome,{user?.email}</h1>
             </>
           } />
+
+          <Route path="/problems"
+        element={
+          <ProtectedRoute>
+            <>
+          <h1 className="text-center mt-6 text-gray-600">Welcome,{user?.email}</h1>
+            </>
+            <ProblemsList/>
+          </ProtectedRoute>
+        }/>
+
         </Route>
+
+        
+
+        
 
     </Routes>
    
