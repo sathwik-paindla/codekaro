d916c7c3e95d6a346760553b4317bdc36c669e3e
sathwik-paindla <paindlasathwik@gmail.com>
2025-12-01 20:09:19 +0530

    Added default boilerplate code for each language

---
+++ frontend/src/components/Boilerplate.jsx
@@ -0,0 +1,25 @@
+export const Boilerplate = {
+  cpp: `#include <iostream>
+using namespace std;
+int main() {
+    // read input and implement solution
+    return 0;
+}
+`,
+  c: `#include <stdio.h>
+int main() {
+    // read input and implement solution
+    return 0;
+}
+`,
+  java: `public class Main {
+  public static void main(String[] args) {
+    // read input and implement solution
+  }
+}
+`,
+  python: `# write your solution here
+if __name__ == "__main__":
+    pass
+`,
+};
